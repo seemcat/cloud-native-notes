6d2a71d17fa08c4f2320b0fbfb78a75343436696
seemcat <hello@vimgirl.com>
2018-11-28 09:52:44 -0800

    Sets up a web app in GoLang

---
+++ bird_handlers.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Bird struct {
+	Species     string `json:"species"`
+	Description string `json:"description"`
+}
+
+var birds []Bird
+
+// To get all birds
+func getBirdHandler(w http.ResponseWriter, r *http.Request) {
+	// Convert the "birds" variable to json
+	fmt.Print("birds: ", birds)
+	birdListBytes, err := json.Marshal(birds)
+
+	// If there is an error, print it to the console, and return a server
+	// error response to the user
+	if err != nil {
+		fmt.Println(fmt.Errorf("Error: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// If all goes well, write the JSON list of birds to the response
+	w.Write(birdListBytes)
+}
+
+// To create a new entry of birds
+func createBirdHandler(w http.ResponseWriter, r *http.Request) {
+	// Wtf is r?
+	fmt.Print("rrrrr: ", r)
+
+	// Create a new instance of Bird
+	bird := Bird{}
+
+	// We send all our data as HTML form data
+	// the `ParseForm` method of the request, parses the
+	// form values
+	err := r.ParseForm()
+
+	// In case of any error, we respond with an error to the user
+	if err != nil {
+		fmt.Println(fmt.Errorf("Error: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+
+	}
+
+	// Get the information about the bird from the form info
+	bird.Species = r.Form.Get("species")
+	bird.Description = r.Form.Get("description")
+
+	// Append our existing list of birds with a new entry
+	birds = append(birds, bird)
+
+	// Is birds an array of objects? Bet ya it is ;)
+	fmt.print("birds: ", birds)
+
+	// Finally, we redirect the user to the original HTMl page
+	// (located at `/assets/`), using the http libraries `Redirect` method
+	http.Redirect(w, r, "/assets/", http.StatusFound)
+
+}
