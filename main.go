6b77591a69e655f9f34d53f0186808f3c4d8f614
seemcat <hello@vimgirl.com>
2018-12-04 00:48:41 -0800

    Made trememndous updates.

--- main.go
+++ main.go
@@ -1,41 +1,64 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"net"
 	"net/http"
+	"net/url"
+	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", handler).Methods("GET")
 
-	// Declare the static file directory and point it to the
-	// directory we just made
 	staticFileDirectory := http.Dir("./assets/")
-
-	// Declare the handler, that routes requests to their respective filename.
-	// The fileserver is wrapped in the `stripPrefix` method, because we want to
-	// remove the "/assets/" prefix when looking for files.
-	// For example, if we type "/assets/index.html" in our browser, the file server
-	// will look for only "index.html" inside the directory declared above.
-	// If we did not strip the prefix, the file server would look for
-	// "./assets/assets/index.html", and yield an error
 	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
-
-	// The "PathPrefix" method acts as a matcher, and matches all routes starting
-	// with "/assets/", instead of the absolute route itself
 	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
 
-	// ToDo: Write tests for these handlers
-	r.HandleFunc("/bird", getBirdHandler).Methods("GET")
-	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
+	r.HandleFunc("/notes", getNoteHandler).Methods("GET")
+	r.HandleFunc("/notes", createNoteHandler).Methods("POST")
 	return r
 }
 
 func main() {
+	fmt.Println("Starting server...")
+	s := os.Getenv("JAWSDB_URL")
+	u, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+
+	user := u.User.Username()
+	password, _ := u.User.Password()
+	host, port, _ := net.SplitHostPort(u.Host)
+	dbname := strings.Replace(u.Path, "/", "", -1)
+
+	connString := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", user, password, host, port, dbname)
+
+	fmt.Println("connString: ", connString)
+
+	db, err := sql.Open(u.Scheme, connString)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	InitStore(&dbStore{db: db})
+
 	r := newRouter()
+	fmt.Println("Serving on port 8080")
 	http.ListenAndServe(":8080", r)
 }
 
