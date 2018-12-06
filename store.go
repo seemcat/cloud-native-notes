9b6836d8499d13497a94de1632cc11540c91bc20
Maricris <seemcat@users.noreply.github.com>
2018-12-06 14:26:56 -0800

    Update store.go

--- store.go
+++ store.go
@@ -1,24 +1,30 @@
 package main
 
-// we'll need the sql go library to play with the db
+// we'll need the sql go library to play with the db.
 import (
 	"database/sql"
 )
 
-// methods of our store
-// if something goes wrong on either of them, an error is returned
+// methods of our store. if something goes wrong on
+// either of them, an error is returned. interfaces are 
+// named collections of method signatures. any type 
+// that defines 'CreateNote' or 'GetNotes' method is 
+// said to satisfy the Store interface.
 type Store interface {
 	CreateNote(note *Note) error
 	GetNotes() ([]*Note, error)
 }
 
-// 'dbStore' struct will implement the 'Store' interface
-// it takes in the sql DB connection object, which represents the db connection
+// since there's no implements keyword in Go, we'll need to
+// create a 'dbStore' type that satisfies the Store interface
+// it takes in the sql DB connection object, which represents 
+// the db connection.
 type dbStore struct {
 	db *sql.DB
 }
 
 // 'Note' is a simple struct with 'title', 'note', 'handle' & 'urlhandle' attributes
+// 'CreateNote' has a receiver type of dbStore
 func (store *dbStore) CreateNote(note *Note) error {
 
 	// the first underscore means that we don't care about what's returned from
@@ -63,7 +69,7 @@ var store Store
 /*
 we'll need to call the InitStore method to initialize the store. this will
 typically be done at the beginning of our application (in this case, when the server starts up)
-*/
+*/ 
 func InitStore(s Store) {
 	store = s
 }
